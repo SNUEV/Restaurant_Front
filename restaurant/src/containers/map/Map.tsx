d000333a3eb355567c1917fcf010cf95a918e30e
sckimynwa <tsb03672@naver.com>
2019-12-31 16:59:54 +0900

    kakao map feature added

---
+++ restaurant/src/containers/map/Map.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './Map.scss';
+
+export class Map extends Component {
+
+  render() {
+    return(
+      <div className="default_map"></div>
+    );
+  }
+
+  componentDidMount() {
+
+  }
+
+}
+
