e8eb302aec9e358949d43d5048b20535224dd20f
sckimynwa <tsb03672@naver.com>
2020-01-21 15:20:57 +0900

    kakao map added

--- restaurant/src/app/App.tsx
+++ restaurant/src/app/App.tsx
@@ -14,7 +14,6 @@ function App() {
       <List></List>
       <MainMap></MainMap>
     </div>
-
   );
 }
 
--- restaurant/src/app/containers/map/Map.tsx
+++ restaurant/src/app/containers/map/Map.tsx
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 
 import './Map.scss';
 
+declare var daum: any
 export class MainMap extends Component {
 
   render() {
     return(
-      <div>This is main map</div>
+      <div id="map">This is main map</div>
     );
   }
 
   componentDidMount() {
-  }
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=d0109d48896c3978a0177697fbc0e3bb&autoload=false&libraries=services";
+    document.head.appendChild(script);
+    const el = document.getElementById('map');
 
+    script.onload = () => {
+      if (el) {
+        daum.maps.load(() => {
+          const option = {
+            center: new daum.maps.LatLng(37.481417, 126.952778),
+            level: 4
+          };
+          const map = new daum.maps.Map(el, option);
+        })
+      }
+    }
+  }
 }
 
 // { lat: 37.481417, lng: 126.952778}}
+// key : d0109d48896c3978a0177697fbc0e3bb
