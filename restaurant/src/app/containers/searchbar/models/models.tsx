8402d7ea8325c2ec79a2f30077db12761b74e17d
sckimynwa <tsb03672@naver.com>
2020-01-21 17:56:46 +0900

    searchbar: action, saga, api 연동

---
+++ restaurant/src/app/containers/searchbar/models/models.tsx
@@ -0,0 +1,10 @@
+export interface Category {
+  id: number,
+  name: string,
+}
+
+export interface Pricerange {
+  id: number,
+  min: number,
+  max: number,
+}
--- restaurant/src/index.tsx
+++ restaurant/src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+
 import './index.scss';
 import App from './app/App';
-import { createStore } from 'redux';
 import reducers from './app/reducers';
-import { Provider } from 'react-redux';
+import rootSaga from './services/sagas';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware),
+);
+
+// sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store = {store}>
