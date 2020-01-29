7306d127fe66aebda2eb2cd3b0c1f310dee548ad
yankee624 <96kichang@naver.com>
2020-01-29 19:37:43 +0900

    list: basic design, action, reducer, saga

---
+++ restaurant/src/app/containers/list/reducers/reducer.tsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { List } from '../List';
+
+const mapStateToProps = (state: any) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(List)
